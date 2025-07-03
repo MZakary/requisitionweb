4e3330eda8c4e78506d5494ee4c5d390a9e8e866
MZakary <zakary-mitrovic@hotmail.ca>
2025-07-03 09:36:31 -0400

    removed unnessecary stuff

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { AppLayout } from './app/layout/component/app.layout';
 import { Dashboard } from './app/pages/dashboard/dashboard';
-import { Documentation } from './app/pages/documentation/documentation';
-import { Landing } from './app/pages/landing/landing';
 import { Notfound } from './app/pages/notfound/notfound';
 import { RequisitionJSON } from './app/pages/RequisitionJSON/RequisitionJSON';
 
@@ -12,8 +10,6 @@ export const appRoutes: Routes = [
         component: AppLayout,
         children: [
             { path: '', component: Dashboard },
-            { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
-            { path: 'documentation', component: Documentation },
             { path: 'requisition-json-externe', component: RequisitionJSON },
             { path: 'requisition-json-interne', component: RequisitionJSON },
             { path: 'requisition-json-scolaire', component: RequisitionJSON },
@@ -24,8 +20,6 @@ export const appRoutes: Routes = [
             { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') }
         ]
     },
-    { path: 'landing', component: Landing },
     { path: 'notfound', component: Notfound },
-    { path: 'auth', loadChildren: () => import('./app/pages/auth/auth.routes') },
     { path: '**', redirectTo: '/notfound' }
 ];
